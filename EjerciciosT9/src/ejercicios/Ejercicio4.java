e383b7a9a71fe676ab5f06d111c7574ee49bb80e
angelnavarromesas <angelnavarromesas02@gmail.com>
2023-05-22 17:11:33 +0200

    comit final

--- EjerciciosT9/src/ejercicios/Ejercicio4.java
+++ EjerciciosT9/src/ejercicios/Ejercicio4.java
@@ -1,25 +1,58 @@
 package ejercicios;
 
+import javax.swing.tree.TreeCellRenderer;
 import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Ejercicio4 {
-	
-	static TreeSet<String> lista = new TreeSet();
-	public static void main(String[] args) {
-		pedirNumeros();
-		System.out.println(lista);
-	}
-	
-	public static void pedirNumeros() {
+    
+    public static void main(String[] args) {
+		TreeMap<Integer, Integer> numeros = new TreeMap();
+		TreeMap<Integer, Integer> estrellas = new TreeMap();
+		int numero;
+		int estrella;
+		String seguir="";
 		Scanner sc = new Scanner(System.in);
-		int num;
 		
-	}
-	
-	public static void pedirEstrellas() {
-		Scanner sc = new Scanner(System.in);
-		int est;
+		System.out.println("Quiere seguir introduciendo números?s/n");
+		seguir = sc.nextLine();
 		
+		while(seguir.equalsIgnoreCase("s")) {
+			
+			System.out.println("Introduzca 5 números");
+			
+			for(int i=1; i<=5; i++) {
+				numero = sc.nextInt();
+				sc.nextLine();
+				
+				if(numeros.containsKey(numero)) {
+					numeros.replace(numero, numeros.get(numero)+1);
+				} else {
+					numeros.put(numero, 1);
+				}
+				
+			}
+			System.out.println("Introduzca 2 estrellas");
+			
+			for(int i=1; i<=2; i++) {
+				estrella = sc.nextInt();
+				sc.nextLine();
+				
+				if(estrellas.containsKey(estrella)) {
+					estrellas.replace(estrella, estrellas.get(estrella)+1);
+				} else {
+					estrellas.put(estrella, 1);
+				}
+				
+			}
+			
+			System.out.println("Quiere seguir introduciendo números?s/n");
+			seguir = sc.nextLine();
+			
+		}
+		
+		System.out.println("Números: " + numeros);
+		System.out.println("Estrellas: " + estrellas);
 	}
 }
--- EjerciciosT9/src/ejercicios/Ejercicio5.java
+++ EjerciciosT9/src/ejercicios/Ejercicio5.java
@@ -1,11 +1,59 @@
 package ejercicios;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Ejercicio5 {
-
-	public static void main(String[] args) {
-		
-		
-		
-	}
-
+    static HashMap<String, Double> lista = new HashMap<>();
+    static Scanner sc =  new Scanner(System.in);
+    public static void main(String[] args) {
+        menu();
+    }
+    public static void menu(){
+        int opc;
+        do {
+            System.out.println("PRODUCTOS");
+            System.out.println("1. Alta de producto");
+            System.out.println("2. Baja de producto");
+            System.out.println("3. Listar existencias");
+            System.out.println("4. Salir.");
+            opc = sc.nextInt();
+            switch (opc){
+                case 1:
+                    alta();
+                    break;
+                case 2:
+                    baja();
+                    break;
+                case 3:
+                    listar();
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }while (opc != 4);
+    }
+    public static void alta(){
+        String nombre;
+        double precio;
+        sc.nextLine();
+        System.out.println("Introduzca el nombre y el precio del producto");
+        nombre = sc.nextLine();
+        precio = sc.nextDouble();
+        lista.put(nombre, precio);
+    }
+    public static void baja(){
+        String nombre;
+        sc.nextLine();
+        System.out.println("Introduzca el nombre del producto");
+        nombre = sc.nextLine();
+        Double hecho = lista.remove(nombre);
+        if (hecho == null)
+            System.out.println("No se ha podido dar de baja al producto especificado");
+    }
+    public static void listar(){
+        System.out.println(lista);
+    }
 }
